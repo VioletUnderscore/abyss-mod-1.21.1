b133b3f81ac9f0e0882541f92d4695994da2ac54
VioletUnderscore <violetunderscore@gmail.com>
2024-09-27 20:31:29 -0600

    indev_24-09-27b
    +Added Dark Quartz Ore
    +Updated Metadata

--- src/main/java/net/violetunderscore/abyss/AbyssMod.java
+++ src/main/java/net/violetunderscore/abyss/AbyssMod.java
@@ -2,6 +2,7 @@ package net.violetunderscore.abyss;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.violetunderscore.abyss.block.ModBlocks;
 import net.violetunderscore.abyss.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,8 @@ public class AbyssMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/violetunderscore/abyss/block/ModBlocks.java
@@ -0,0 +1,38 @@
+package net.violetunderscore.abyss.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.violetunderscore.abyss.AbyssMod;
+
+public class ModBlocks {
+
+    public static final Block DARK_QUARTZ_ORE = registerBlock("dark_quartz_ore",
+            new Block(AbstractBlock.Settings.create().strength(3.0F, 9.0F).sounds(BlockSoundGroup.STONE).mapColor(MapColor.PALE_YELLOW)));
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(AbyssMod.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(AbyssMod.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+
+    }
+    public static void registerModBlocks() {
+        AbyssMod.LOGGER.info("Registering Mod Blocks For " + AbyssMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(DARK_QUARTZ_ORE);
+        });
+    }
+}
